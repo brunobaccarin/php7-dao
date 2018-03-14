91ca01a6537563cd3d4bf0da203a0f1a82670b36
Bruno Baccarin <brunobaccarin01@gmail.com>
2018-03-14 09:46:00 -0300

    metodos de lista

--- class/Usuario.php
+++ class/Usuario.php
@@ -42,7 +42,7 @@ class Usuario {
 
 	public function loadById($id){
 
-		$sql = new sql();
+		$sql = new Sql();
 
 		$results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(
 			":ID"=>$id
@@ -58,13 +58,54 @@ class Usuario {
 		}
 	}
 
+	public static function getList(){
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin;");
+	}
+
+	public static function search($login){
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+
+			':SEARCH'=>"%".$login."%"
+		));
+	}
+
+	public function login($login, $password){
+
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN and dessenha = :PASSWORD", array(
+			":LOGIN"=>$login,
+			":PASSWORD"=>$password
+		));
+
+		if (count($results) > 0){
+			$row = $results[0];
+
+			$this->setIdusuario($row['idusuario']);
+			$this->setDeslogin($row['deslogin']);
+			$this->setDessenha($row['dessenha']);
+			$this->setDescadastro(new DateTime ($row['descadastro']));
+		} else {
+
+			throw new Exception("Login e/ou senha inválidos.");
+			
+		}
+
+	}
+
 	public function __toString(){
 
 		return json_encode(array(
 			"idusuario"=>$this->getIdusuario(),
 			"deslogin"=>$this->getDeslogin(),
 			"dessenha"=>$this->getDessenha(),
-			"descadastro"=>$this->getDescadastro()->format("d/m/Y H:i:s")
+			"descadastro"=>$this->getDescadastro()->format("d/m/Y  H:i:s")
 		));
 	}
 
--- index.php
+++ index.php
@@ -2,10 +2,25 @@
 
  require_once("config.php");
 
-$bruno = new Usuario();
 
-$bruno->loadbyId(1);
+//Carrega 1 usuario
+//$bruno = new Usuario();
+//$bruno->loadbyId(1);
+ //echo $bruno;
 
-echo $bruno;
 
+//Carrega uma lista de usuarios
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+
+//Carrega uma lista de usuarios buscando pelo login
+//$search = Usuario::search("G");
+//echo json_encode($search);
+
+ //Carrega usuario usando login e senha
+$usuario = new Usuario();
+$usuario->login("Gabriel", "abcde");
+
+echo $usuario;
  ?>
